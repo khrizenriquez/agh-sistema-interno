f19b74d90cdacc76463aa36b3dab6cc88e38236c
Khriz Enríquez <christoferen7@gmail.com>
2015-09-07 11:59:03 -0600

    Agregando función que regresa la fecha actual, servirá para tenerla en una clase greneral y hacer uso de ella en cualquier método

--- src/configurations/Generals.java
+++ src/configurations/Generals.java
@@ -1,17 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
  */
 package configurations;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
- *
- * @author khriz
+ * @author Duku
  */
-public class GeneralVariables {
+public class Generals {
     public static DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    
+    /**
+    * Regresando la fecha actual, puede servir para inserciones en la bd o para obtener la fecha actual
+    * con formato [año]/[mes]/[dia] [hora]:[minutos]:[segundos]
+    * 
+    * @return a date in string format
+    */
+    public String getActualDate () {
+        Date date = new Date();
+
+        return dateFormat.format(date);
+    }
 }
