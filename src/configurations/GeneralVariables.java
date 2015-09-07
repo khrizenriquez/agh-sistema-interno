8ac8ead924f6915ac8a1efdcb98fb414d7827844
Khriz Enríquez <christoferen7@gmail.com>
2015-09-07 10:37:44 -0600

    Configuración de clase, donde estrarán todas las variables necesarias para usarse en toda la aplicación

---
+++ src/configurations/GeneralVariables.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package configurations;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author khriz
+ */
+public class GeneralVariables {
+    public static DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+}
