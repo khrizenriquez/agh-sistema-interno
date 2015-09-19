1f0445186f492ba494988a4ac6da72a25c0647e3
Khriz Enríquez <christoferen7@gmail.com>
2015-09-19 11:05:11 -0600

    Limpiando main para lanzar ventana de login

--- src/intranet/Intranet.java
+++ src/intranet/Intranet.java
@@ -20,35 +20,9 @@ public class Intranet extends Application {
 
         Scene scene = new Scene(root);
 
-//        stage.setScene(scene);
-//        stage.setResizable(false);
-//        stage.show();
-        //http://www.jc-mouse.net/base-de-datos/sqlitejava-conexion
-        
-        String controller   = "org.sqlite.JDBC";
-        String databaseName = "agh-sistema-interno.sqlite";
-        String path         = "jdbc:sqlite:src/configurations/" + databaseName;
-        Statement statement = null;
-        Connection conn = null;
-        try {
-            Class.forName(controller);
-            conn = (Connection) DriverManager.getConnection(path);
-        } catch (Exception ex) {
-            System.out.println("Error" + ex.getMessage());
-        }
-        
-        String query = "SELECT * FROM user where user = 'khrizenriquez' and password = '12345678';";
-        ResultSet resultSet = (ResultSet) conn.createStatement();
-        resultSet = statement.executeQuery("SELECT * FROM persona ; ");
-        while (resultSet.next()) {
-            System.out.println(resultSet.getRow());
-        }
-        //PreparedStatement st = conn.prepareStatement(query);
-        /*
-         PreparedStatement pstm = connection.prepareStatement(q);
-        pstm.execute();
-        pstm.close();
-        */
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 
     /**
